56d6b99d670326c4289692f17dcbafdd6aea558d
Riedhwaan <99826417+Riedhwaan@users.noreply.github.com>
2022-02-27 21:08:13 +0200

    Add files via upload

---
+++ Question_2.1/Question_2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Question_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox_1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Translate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBox_1
+            // 
+            this.txtBox_1.Location = new System.Drawing.Point(241, 117);
+            this.txtBox_1.Name = "txtBox_1";
+            this.txtBox_1.Size = new System.Drawing.Size(207, 22);
+            this.txtBox_1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter a word";
+            // 
+            // btn_Translate
+            // 
+            this.btn_Translate.Location = new System.Drawing.Point(216, 220);
+            this.btn_Translate.Name = "btn_Translate";
+            this.btn_Translate.Size = new System.Drawing.Size(148, 23);
+            this.btn_Translate.TabIndex = 2;
+            this.btn_Translate.Text = "Translate";
+            this.btn_Translate.UseVisualStyleBackColor = true;
+            this.btn_Translate.Click += new System.EventHandler(this.btn_Translate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 317);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_Translate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox_1);
+            this.Name = "Form1";
+            this.Text = "Pig Latin Translator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox_1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Translate;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Question_2.1/Question_2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Translate_Click(object sender, EventArgs e)
+        {
+            string word, pig_Latin;
+            word = txtBox_1.Text; //the word in the textbox is retreived
+            pig_Latin = word.Substring(1, word.Length - 1) + word.Substring(0, 1) + "ay"; //conversion of the word
+            label2.Text = ("The pig latin version is ") + pig_Latin; //the pig latin version displayed
+        }
+    }
+}
